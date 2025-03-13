662830711e5b7220abb906ef6bd5c35ea19c7ce5
Juan <juan-torres29@hotmail.es>
2025-03-13 09:55:04 +0100

    Arreglos en el archivo de la base de datos y en la clase CitaServicio

--- includes/database.php
+++ includes/database.php
@@ -10,7 +10,7 @@ $db->set_charset('utf8');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
-    echo "error de depuración: " . mysqli_connect_errno();
+    echo "errno de depuración: " . mysqli_connect_errno();
     echo "error de depuración: " . mysqli_connect_error();
     exit;
 }
